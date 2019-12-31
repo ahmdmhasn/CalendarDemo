0cca69970e71c842186766c0caef57e6077cb422
Ahmed M. Hassan <ahassan@victorylink.com>
2019-12-31 13:28:10 +0200

    Initial Commit

---
+++ ReminderApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReminderApp
+//
+//  Created by Victory Mac Mini on 12/31/19.
+//  Copyright © 2019 Victory Mac Mini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
